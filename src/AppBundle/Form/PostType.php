89e25721273ade08e70ddc20d6c0d98437cd3fe0
ricardohenrique <ricardo.hb@outlook.com>
2018-12-23 23:34:24 -0300

    associate form with post

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,7 +18,9 @@ class PostType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults([
+            'data_class' => Post::class
+        ]);
     }
 
     public function getBlockPrefix()
